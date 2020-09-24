f6c695bc48ecccbf4bc9acf13568d9da530f0a3d
TanishqGupta11 <tanishqgpt116@gmail.com>
2020-09-24 19:18:42 +0530

    Added user functionality such as add-to-cart, update-cart, checkout, basically wrote code in views.py to get cartItems

--- store/models.py
+++ store/models.py
@@ -61,7 +61,7 @@ class OrderItem(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     @property
-    def get_total(self):
+    def get_total(self): 
         total = self.product.price * self.quantity
         return total
 
